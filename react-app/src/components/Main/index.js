5c25b62143c67e6451be0ad1d2ab4cef08b66970
Rick Arocho <rick.arocho@gmail.com>
2021-12-16 01:16:24 -0600

    styled first draft of splash page

--- react-app/src/components/Main/index.js
+++ react-app/src/components/Main/index.js
@@ -7,17 +7,32 @@ import heroImage from './assets/hero-image.svg';
 const Main = () => {
 
     return (
-        <header>
-            <div className={style.heroWrapper}>
-                <div className={style.headerContent}>
-                    <h2>Jello helps teams move work forward.</h2>
-                    <Link to={'/sign-up'} className={style.mainBtn}>Sign up - it's free!</Link>
+        <div>
+            <header className={style.mainHeader}>
+                <div className={style.heroWrapper}>
+                    <div className={style.headerContent}>
+                        <h2>Jello helps teams move work forward.</h2>
+                        <Link to={'/sign-up'} className={style.mainBtn}>Sign up - it's free!</Link>
+                    </div>
+                    <div className={style.headerImage}>
+                        <img src={heroImage} alt='Hero Image' />
+                    </div>
                 </div>
-                <div className={style.headerImage}>
-                    <img src={heroImage} alt='Hero Image' />
+            </header>
+            <section className={style.mainSection}>
+                <h2>It’s more than work. It’s a way of working together.</h2>
+                <p>Start with a Jello board, lists, and cards. Customize and expand with more features as your teamwork grows. Manage projects, organize tasks, and build team spirit—all in one place.</p>
+                <Link to={'/sign-up'} className={style.mainBtn}>Sign up - it's free!</Link>
+            </section>
+            <section className={style.productShowcaseLarge}>
+                <div>
+                    <h2>Image Placeholder</h2>
                 </div>
-            </div>
-        </header>
+            </section>
+            <footer>
+                <p>Created By: Author</p>
+            </footer>
+        </div>
     )
 };
 
