d620087b3c132d96eee768c59bd1bdafa42a8a77
Rick Arocho <rick.arocho@gmail.com>
2021-12-15 19:07:42 -0600

    started main nav styling

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -2,35 +2,40 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import style from "./navbar.module.css";
 
 const NavBar = () => {
   return (
     <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            <h1>Logo</h1>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        {/* <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li> */}
-      </ul>
+      <div className={style.mainNavWrapper}>
+        <ul>
+          <li>
+            <NavLink to='/' exact={true} activeClassName='active'>
+              LOGO
+            </NavLink>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <NavLink to='/login' exact={true} activeClassName='active'>
+              Log in
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/sign-up' exact={true} className={style.mainBtn}>
+              Sign up
+            </NavLink>
+          </li>
+          {/* <li>
+            <NavLink to='/users' exact={true} activeClassName='active'>
+              Users
+            </NavLink>
+          </li>
+          <li>
+            <LogoutButton />
+          </li> */}
+        </ul>
+      </div>
     </nav>
   );
 }
