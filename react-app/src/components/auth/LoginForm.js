781bb624f3317a1eff10d175306e86e4110971a2
Rick Arocho <rick.arocho@gmail.com>
2021-12-16 16:08:30 -0600

    finished styling login page

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 import { login } from '../../store/session';
 import style from './auth.module.css'
 
@@ -33,32 +33,40 @@ const LoginForm = () => {
 
   return (
     <div className={style.authWrapper}>
-      <form onSubmit={onLogin}>
-        <div>
-          {errors.map((error, ind) => (
-            <div key={ind}>{error}</div>
-          ))}
-        </div>
-        <div className={style.inputWrapper}>
-          <label htmlFor='email'>Email</label>
-          <input
-            name='email'
-            type='text'
-            value={email}
-            onChange={updateEmail}
-          />
-        </div>
-        <div className={style.inputWrapper}>
-          <label htmlFor='password'>Password</label>
-          <input
-            name='password'
-            type='password'
-            value={password}
-            onChange={updatePassword}
-          />
-          <button type='submit' className={style.mainBtn}>Log in</button>
-        </div>
-      </form>
+      <div>
+        <Link to={'/'} className={style.formLogo}>
+          <h2>Logo</h2>
+        </Link>
+        <form onSubmit={onLogin}>
+          <div className={style.formErrors}>
+            {errors.map((error, ind) => (
+              <div key={ind}>{error}</div>
+            ))}
+          </div>
+          <div className={style.inputWrapper}>
+            <label htmlFor='email'>Email</label>
+            <input
+              name='email'
+              type='text'
+              value={email}
+              onChange={updateEmail}
+            />
+          </div>
+          <div className={style.inputWrapper}>
+            <label htmlFor='password'>Password</label>
+            <input
+              name='password'
+              type='password'
+              value={password}
+              onChange={updatePassword}
+            />
+            <Link to={'/sign-up'}>
+              <p>Don't have an account? Sign up</p>
+            </Link>
+            <button type='submit' className={style.mainBtn}>Log in</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
