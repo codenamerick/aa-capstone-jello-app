016b61b4d8ddd610daefc12a9af41488e4b05fff
Rick Arocho <rick.arocho@gmail.com>
2021-12-16 00:00:30 -0600

    styled nav/header

--- react-app/src/components/Main/index.js
+++ react-app/src/components/Main/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, Link } from 'react-router-dom';
+import style from "./Main.module.css";
+import heroImage from './assets/hero-image.svg';
 
 const Main = () => {
 
-  return (
-    <div>
-        <h1>Home</h1>
-    </div>
-  )
+    return (
+        <header>
+            <div className={style.heroWrapper}>
+                <div className={style.headerContent}>
+                    <h2>Jello helps teams move work forward.</h2>
+                    <Link to={'/sign-up'} className={style.mainBtn}>Sign up - it's free!</Link>
+                </div>
+                <div className={style.headerImage}>
+                    <img src={heroImage} alt='Hero Image' />
+                </div>
+            </div>
+        </header>
+    )
 };
 
 
