4c1b838b251c7848cd4c59b6deef27e3a59ebf09
Rick Arocho <rick.arocho@gmail.com>
2021-12-17 16:16:01 -0600

    added list_id to card model

--- app/models/card.py
+++ app/models/card.py
@@ -10,10 +10,12 @@ class Card(db.Model):
     description = db.Column(db.Text)
     due_date = db.Column(db.DateTime(), server_default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('lists.id'), nullable=False)
     created_at = db.Column(db.DateTime(), nullable=False, server_default=func.now())
     updated_at = db.Column(db.DateTime(), onupdate=func.now(), default=func.now())
 
     users = db.relationship('User', back_populates='cards')
+    lists = db.relationship('List', back_populates='cards')
     assignments = db.relationship('Assignment', back_populates='cards', cascade='all, delete')
     comments = db.relationship('Comment', back_populates='cards', cascade='all, delete')
 
@@ -24,6 +26,7 @@ class Card(db.Model):
             'description': self.description,
             'due_date': self.due_date,
             'user_id': self.user_id,
+            'list_id': self.list_id,
             'assignments': [user.id for user in self.assignments],
             'assignment_list': [user.to_dict() for user in self.assignments],
             'created_at': self.created_at.strftime('%m/%d/%Y %H:%M:%S'),
