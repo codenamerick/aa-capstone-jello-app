5a524c6561b2dc69f9901b39923ac1474136c16d
Rick Arocho <rick.arocho@gmail.com>
2021-12-31 20:00:58 -0600

    added and started styling for card details

--- react-app/src/components/CardsContainer/index.js
+++ react-app/src/components/CardsContainer/index.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import style from './CardsContainer.module.css';
+import { MainModal } from "../../context/MainModal";
+import CardDetails from "./CardDetails";
 
 const CardsContainer = ({list}) => {
+    const [showMainModal, setShowMainModal] = useState(false);
 
     return (
         <div className={style.CardsContainer}>
             {list.cards?.map((card) => (
-                <div key={card.id} className={style.card}>
+                <div key={card.id} className={style.card} onClick={() => setShowMainModal(true)}>
                     <p>{card.name}</p>
                 </div>
             ))}
+            {showMainModal && (
+                <MainModal onClose={() => setShowMainModal(false)}>
+                    <CardDetails setShowMainModal={setShowMainModal} list={list} />
+                </MainModal>
+            )}
         </div>
     );
 };
