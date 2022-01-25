77d1f20aaa6d226196751d5270067adda78cdb84
Rick Arocho <rick.arocho@gmail.com>
2022-01-24 19:00:39 -0600

    added new constraints to my list model to include list order

--- app/models/list.py
+++ app/models/list.py
@@ -6,12 +6,13 @@ import datetime
 class List(db.Model):
     __tablename__ = 'lists'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     board_id = db.Column(db.Integer, db.ForeignKey('boards.id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     created_at = db.Column(db.DateTime(), nullable=False, server_default=func.now())
     updated_at = db.Column(db.DateTime(), onupdate=func.now(), default=func.now())
+    list_order = db.Column(db.Integer, nullable=False)
 
     boards = db.relationship('Board', back_populates='lists')
     cards = db.relationship('Card', back_populates='lists', cascade='all, delete')
@@ -25,5 +26,6 @@ class List(db.Model):
             'board_id': self.board_id,
             'user_id': self.user_id,
             'created_at': self.created_at.strftime('%m/%d/%Y %H:%M:%S'),
-            'updated_at': self.updated_at.strftime('%m/%d/%Y %H:%M:%S')
+            'updated_at': self.updated_at.strftime('%m/%d/%Y %H:%M:%S'),
+            'list_order': self.list_order
         }
