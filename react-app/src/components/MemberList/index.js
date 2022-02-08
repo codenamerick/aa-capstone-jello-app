4e42957d97e350eff3fcda09ad7cef0127703585
Rick Arocho <rick.arocho@gmail.com>
2022-02-08 11:04:01 -0600

    added tooltips to members on screens larger than 720px

--- react-app/src/components/MemberList/index.js
+++ react-app/src/components/MemberList/index.js
@@ -5,14 +5,12 @@ import "./MemberList.css";
 const MemberList = ({boardId}) => {
     const boardMembers = useSelector(state => state.boards[boardId].member_list);
 
-    console.log('BOARD MEMBERS-----: ', boardMembers)
-
     return (
         <div className='memberListWrapper'>
             {boardMembers.map(member => (
-                <div key={member.id} className='memberListAvatar'>
+                <div key={member.id} className='memberListAvatar' id={`member-${member.id}`}>
                     {member.username[0]}
-                    <div className='memberTooltip' id={`member-tooltip-${member.id}`}>{member.username}</div>
+                    <div className={`memberTooltip`} id={`member-tooltip-${member.id}`}>{member.username}</div>
                 </div>
             ))}
         </div>
